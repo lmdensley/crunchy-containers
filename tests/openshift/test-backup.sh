9a18b0e70842aa2f6ca078e2613491b2db84fb45
jeffmc <jeffmc@ose4jeff.crunchy.lab>
2016-03-28 12:14:11 -0400

    add backup test for openshift

--- tests/openshift/test-backup.sh
+++ tests/openshift/test-backup.sh
@@ -12,6 +12,9 @@ oc delete pv backup-job-pv
 oc delete job backup-job
 oc delete pod single-master
 oc delete service single-master
+export PGPASSFILE=/tmp/single-master-pgpass
+chmod 777 $PGPASSFILE
+/usr/bin/rm $PGPASSFILE
 echo "sleeping for 20 seconds to allow any existing pods/services to terminate"
 
 sleep 40
@@ -25,27 +28,16 @@ echo $IP " is the IP address"
 export MASTERPSW=`oc describe pod single-master | grep MASTER_PASSWORD | cut -f2 -d':' | xargs`
 echo "["$MASTERPSW"] is the master password"
 
-export PGPASSFILE=/tmp/single-master-pgpass
-chmod 777 $PGPASSFILE
-/usr/bin/rm $PGPASSFILE
 
 echo "creating PGPASSFILE..."
 echo "*:*:*:*:"$MASTERPSW > $PGPASSFILE
 chmod 400 $PGPASSFILE
-
-export LOCAL_IP=`/sbin/ifconfig -a | awk '/(cast)/ { print $2 }' | cut -d':' -f2 | tail -1`
-
-echo "local ip address is " $LOCAL_IP
 
 psql -h $IP -U master postgres -c 'select now()'
 
 rc=$?
 
 echo $rc is the rc
-
-chmod 777 $PGPASSFILE
-/usr/bin/rm $PGPASSFILE
-# always delete the pod and service even on a failure
 
 if [ 0 -eq $rc ]; then
 	echo "connection test passed"
@@ -54,21 +46,41 @@ else
 	exit $rc
 fi
 
-cp backup-job.json temp-backup-job.json
-cp backup-job-pv.json temp-backup-job-pv.json
-sed -i "s/REPLACE_PASSWORD/$MASTERPSW/g" temp-backup-job.json
-sed -i "s/REPLACE_IP_ADDRESS/$LOCAL_IP/g" temp-backup-job-pv.json
+export LOCAL_IP=`ifconfig | grep inet | head -1 | xargs | cut -f2 -d' '`
 
-cat temp-backup-job.json
-cat temp-backup-job-pv.json
+echo "local ip address is " $LOCAL_IP
 
-oc create -f temp-backup-job-pv.json
+cp backup-job.json /tmp/temp-backup-job.json
+cp backup-job-pv.json /tmp/temp-backup-job-pv.json
+sed -i "s/REPLACE_PASSWORD/$MASTERPSW/g" /tmp/temp-backup-job.json
+sed -i "s/REPLACE_IP_ADDRESS/$LOCAL_IP/g" /tmp/temp-backup-job-pv.json
+
+cat /tmp/temp-backup-job.json
+cat /tmp/temp-backup-job-pv.json
+
+# clear out any previous backups
+sudo rm -rf /nfsfileshare/single-master
+
+oc create -f /tmp/temp-backup-job-pv.json
 oc create -f backup-job-pvc.json
-oc create -f temp-backup-job.json
+oc create -f /tmp/temp-backup-job.json
 
 echo "sleep while backup executes"
 sleep 30
 
 oc delete pod single-master
 oc delete service single-master
+oc delete job backup-job
+oc delete pvc backup-job-pvc
+oc delete pv backup-job-pv
+
+sudo find /nfsfileshare/single-master/ -name "postgresql.conf"
+rc=$?
+echo "final rc is " $rc
+if [ 0 -eq $rc ]; then
+	echo "backup test passed"
+else
+	echo "backup test FAILED"
+	exit $rc
+fi
 exit 0
