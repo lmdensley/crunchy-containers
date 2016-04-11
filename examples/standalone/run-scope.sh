037445c6edc1999cbaba00b7b82f30f8c03ce3c8
jmccormick2001 <djm29492@gmail.com>
2016-04-11 15:00:03 -0400

    add scope run example for running prometheus and grafana in standalone mode

--- examples/standalone/run-scope.sh
+++ examples/standalone/run-scope.sh
@@ -13,29 +13,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "starting dnsbridge container..."
+echo "starting prometheus container..."
 
-docker stop crunchy-dns
-docker rm crunchy-dns
+docker stop crunchy-prometheus
+docker rm crunchy-prometheus
 
-DATA_DIR=/tmp/crunchy-dns
+DATA_DIR=/tmp/crunchy-prometheus
 sudo rm -rf $DATA_DIR
 sudo mkdir -p $DATA_DIR
-sudo chown postgres:postgres $DATA_DIR
+sudo chown root:root $DATA_DIR
 sudo chcon -Rt svirt_sandbox_file_t $DATA_DIR
 
 export HOSTIP=`hostname --ip-address`
 
 sudo docker run \
-	-p $HOSTIP:8500:8500/tcp \
-	-p $HOSTIP:53:8600/udp \
-	-v $DATA_DIR:/consuldata \
-	--privileged \
-	-v /run/docker.sock:/tmp/docker.sock \
-	-e DOCKER_URL=unix:///tmp/docker.sock \
+	-p $HOSTIP:9090:9090/tcp \
+	-p $HOSTIP:9091:9091/tcp \
+	-v $DATA_DIR:/data \
 	-e DC=dc1 \
 	-e DOMAIN=crunchy.lab. \
-	--name=crunchy-dns \
-	--hostname=crunchy-dns \
-	-d crunchydata/crunchy-dns:latest
+	--name=crunchy-prometheus \
+	--hostname=crunchy-prometheus \
+	-d crunchydata/crunchy-prometheus:latest
 
+sudo docker run \
+	-p $HOSTIP:3000:3000/tcp \
+	-v $DATA_DIR:/data \
+	-e DC=dc1 \
+	-e DOMAIN=crunchy.lab. \
+	--link crunchy-prometheus:crunchy-prometheus \
+	--name=crunchy-grafana \
+	--hostname=crunchy-grafana \
+	-d crunchydata/crunchy-grafana:latest
