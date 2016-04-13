22d1f8467d7152b2227bc225199b14f38cc09975
jeffmc <jeffmc@ose4jeff.crunchy.lab>
2016-04-13 10:11:48 -0400

    fix restore test script

--- examples/openshift/master-restore/run.sh
+++ examples/openshift/master-restore/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cat master-restore-pv.json | sed -e 's/IPADDRESS/$IPADDRESS' | oc create -f -
+
+IPADDRESS=`hostname --ip-address`
+cat $BUILDBASE/examples/openshift/master-restore/master-restore-pv.json | sed -e "s/IPADDRESS/$IPADDRESS/g" | oc create -f -
+
 oc create -f master-restore-pvc.json
 oc process -f master-restore.json | oc create -f -
